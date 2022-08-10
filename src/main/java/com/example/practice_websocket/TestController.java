c50598d5975e12ed141c6d9a8a112484ea319825
ikohong88 <ikohong88@gmail.com>
2022-08-11 02:23:36 +0900

    - test Jenkins

--- src/main/java/com/example/practice_websocket/TestController.java
+++ src/main/java/com/example/practice_websocket/TestController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-
+public class testController {
     @GetMapping("/")
-    public String test(){
-        return "hello My Jenkins world!";
+    public String index() {
+        return "Hello Jenkins!";
     }
 }
