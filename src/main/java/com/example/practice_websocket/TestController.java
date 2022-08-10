6d0198a8fc1c698c8ba207fa0ca53204a3ae1e67
ikohong88 <ikohong88@gmail.com>
2022-08-11 02:41:38 +0900

    - Jenkins test

--- src/main/java/com/example/practice_websocket/TestController.java
+++ src/main/java/com/example/practice_websocket/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String index() {
-        return "Hello Jenkins!";
+        return "Hello Jenkins World!";
     }
 }
